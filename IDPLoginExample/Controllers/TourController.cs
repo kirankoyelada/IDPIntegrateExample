51f95be6cb86c0a1d83ef8727edff1d49f17b02e
kiran kumar <kirankoyelada@outlook.com>
2019-06-02 20:52:52 +0530

    Add project files.

---
+++ IDPLoginExample/Controllers/TourController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDPLoginExample.Controllers
+{
+    [Authorize]
+    public class TourController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+    }
+}
